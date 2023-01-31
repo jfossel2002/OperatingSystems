3bddc4a48b9ddc5a6c226fa0566c4241593fa045
Auden Woolfson <auden.woolfson@gmail.com>
2023-01-31 13:25:16 -0500

    gitgitnore, changed class formatting

---
+++ Project-1/Process.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Process {
+
+    int id;
+    int memory;
+    int scheduling_information;
+    int accounting_information;
+    string process_state;
+    string other_resources;
+
+    // pointers will be needed for parent and child processes, open files, class?
+
+    void readFromFile(string path) {
+        ifstream datafileIfstream(path);
+
+    }
+};
