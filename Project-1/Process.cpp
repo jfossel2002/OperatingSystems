805d703ab2187854a1466746e3661470ca82d4e7
Auden Woolfson <auden.woolfson@gmail.com>
2023-02-06 13:03:28 -0500

    pre pull

--- Project-1/Process.cpp
+++ Project-1/Process.cpp
@@ -10,4 +10,22 @@ Process::Process() {};
 
 void Process::loadFromLine(string line) {
     Process::testString = line;
+    vector<string> words;
+    string currentWord = "";
+    for (auto x : line)
+    {
+        if (x == ' ')
+        {
+            words.push_back(currentWord);
+        }
+        else
+        {
+            currentWord = currentWord + x;
+        }
+    }
+
+    string idString = words[1];
+    Process::id = stoi(idString);
+
+    
 }
