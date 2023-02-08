cef1d7fea3bf39f4a9cff6f141d782ba05adfa5e
JFossel <jfossel@conncoll.edu>
2023-02-08 11:28:51 -0500

    Changed so each PCB contains one procces Procces class now obslete. Adding other error handling

--- Project-1/ProcessControlBlock.h
+++ Project-1/ProcessControlBlock.h
@@ -6,7 +6,24 @@ class ProcessControlBlock
 {
 public:
     string name;
-    vector<Process> processes;
     bool readFromFile(string path);
     void printProcesses();
+
+    string fullString;
+
+    int id;
+    int cpu_state;
+    int memory;
+    int scheduling_information;
+    int accounting_information;
+    string process_state;
+    string other_resources;
+    string open_files; // TODO Change type
+
+    int parent;
+    int children;
+
+    void loadFromLine(string line);
+    void to_string();
+    void loadFromLine();
 };
